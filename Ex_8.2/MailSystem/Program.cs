6ea29aba7b329056c147effe828c85d360b718ce
Andrey <Andrey@DESKTOP-9US8I6T>
2016-06-30 10:02:37 +0300

    Removed static

--- Ex_8.2/MailSystem/Program.cs
+++ Ex_8.2/MailSystem/Program.cs
@@ -12,29 +12,25 @@ namespace MailSystem
         public static void Main(string[] args)
         {
             MailManager manager = new MailManager();
-            manager.MailArrived += NewMailArrived;
+            EventHandler<MailArrivedEventArgs> mailHandler = (sender, e) =>
+                {
+                    Console.WriteLine("Mail title is {0} and mail body is {1}", e.Title, e.Body);
+                };
+            manager.MailArrived += mailHandler;
             manager.SimulateMailArrived("Hello", "Good day");
             // Check empty string
             manager.SimulateMailArrived("", "");
             // Check null
             manager.SimulateMailArrived(null, null);
-            manager.MailArrived -= NewMailArrived;
+
             // Timer
-            TimerCallback timerCallBack = new TimerCallback(MailTiming);
+            TimerCallback timerCallBack = (state) =>
+            {
+                manager.SimulateMailArrived("Notice", "Please dont forget your keys!");
+            };
             Timer mailTimer = new Timer(timerCallBack, null, 0, 1000);
             Thread.Sleep(12000);
-        }
-
-        static void NewMailArrived(object sender, MailArrivedEventArgs e)
-        {
-            Console.WriteLine("Mail title is {0} and mail body is {1}", e.Title, e.Body);
-        }
-
-        static void MailTiming(object state)
-        {
-            MailManager timerManager = new MailManager();
-            timerManager.MailArrived += NewMailArrived;
-            timerManager.SimulateMailArrived("Notice", "Please dont forget your keys!");
+            manager.MailArrived -= mailHandler;
         }
     }
 }
