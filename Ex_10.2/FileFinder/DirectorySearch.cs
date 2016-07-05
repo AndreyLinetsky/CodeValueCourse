286d7932977e767f15f3888d8cc8606a228c52c4
Andrey <Andrey@DESKTOP-9US8I6T>
2016-07-05 17:12:04 +0300

    Add trace

---
+++ Ex_10.2/FileFinder/DirectorySearch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace FileFinder
+{
+    public class DirectorySearch
+    {
+        private bool isDirExists = false;
+        public DirectorySearch(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                isDirExists = true;
+            }
+        }
+
+        public bool IsDirExists
+        {
+            get
+            {
+                return isDirExists;
+            }
+        }
+
+        public List<FileInfo> FindFiles(string currDir)
+        {
+            List<FileInfo> files = new List<FileInfo>();
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(currDir);
+                files = dir.GetFiles().ToList<FileInfo>();
+                List<DirectoryInfo> directories = dir.GetDirectories().ToList<DirectoryInfo>();
+                foreach (DirectoryInfo dirInfo in directories)
+                {
+                    if (dirInfo.Exists)
+                    {
+                        files.AddRange(FindFiles(dirInfo.FullName));
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Trace.WriteLine(ex.Message);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Trace.WriteLine(ex.Message);
+            }
+            return files;
+        }
+    }
+}
--- Ex_10.2/FileFinder/Program.cs
+++ Ex_10.2/FileFinder/Program.cs
@@ -4,78 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Diagnostics;
 
 namespace FileFinder
 {
     class Program
     {
-        public List<FileInfo> GetFiles(string currDir)
-        {
-            List<FileInfo> files = new List<FileInfo>();
-            try
-            {
-                DirectoryInfo dir = new DirectoryInfo(currDir);
-                files = dir.GetFiles().ToList<FileInfo>();
-                List<DirectoryInfo> directories = dir.GetDirectories().ToList<DirectoryInfo>();
-                foreach (DirectoryInfo dirInfo in directories)
-                {
-                    if (dirInfo.Exists)
-                    {
-                        files.AddRange(GetFiles(dirInfo.FullName));
-                    }
-                }
-            }
-            catch (IOException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return files;
-        }
-
         static void Main(string[] args)
         {
-            try
+            if (args.Length >= 2 && !String.IsNullOrWhiteSpace(args[0]) &&
+                                    !String.IsNullOrWhiteSpace(args[1]))
             {
-                if (args.Length >= 2 && !String.IsNullOrWhiteSpace(args[0]) &&
-                    !String.IsNullOrWhiteSpace(args[1]))
+                DirectorySearch dirSearch = new DirectorySearch(args[0]);
+                if (dirSearch.IsDirExists)
                 {
-                    if (Directory.Exists(args[0]))
+                    List<FileInfo> files = dirSearch.FindFiles(args[0]);
+                    foreach (FileInfo currFile in files)
                     {
-                        Program prog = new Program();
-                        List<FileInfo> files = prog.GetFiles(args[0]);
-                        foreach (FileInfo currFile in files)
+                        if (currFile.Exists &&
+                            currFile.Name.Contains(args[1]))
                         {
-                            if (currFile.Exists &&
-                                currFile.Name.Contains(args[1]))
-                            {
-                                Console.WriteLine("File name is {0} and file length is {1}", currFile.FullName, currFile.Length);
-                            }
+                            Trace.WriteLine(String.Format("File name is {0} and file length is {1}", currFile.FullName, currFile.Length));
                         }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Directory {0} does not exist", args[0]);
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Error! Please submit the correct path and filter word");
+                    Trace.WriteLine(String.Format("Directory {0} does not exist", args[0]));
                 }
-
             }
-
-            catch (IOException ex)
+            else
             {
-                Console.WriteLine(ex.Message);
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                Console.WriteLine(ex.Message);
+                Trace.WriteLine("Error! Please submit the correct path and filter word");
             }
         }
     }
 }
+
