97b77bcf1c036cfb0e685437a4d7fd6a39ad9336
Andrey <Andrey@DESKTOP-9US8I6T>
2016-07-10 15:07:15 +0300

    FormsTest

---
+++ Backgammon/BackgammonUi/BoardForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackgammonUi
+{
+    public partial class BoardForm : Form
+    {
+        public BoardForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void BoardForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
