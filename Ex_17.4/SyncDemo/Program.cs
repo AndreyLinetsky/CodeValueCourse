3ce12b9ae913bcd3116833a4fa86a6328b20817f
Andrey <Andrey@DESKTOP-9US8I6T>
2016-07-24 15:21:19 +0300

    17.4

---
+++ Ex_17.4/SyncDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SyncDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool mutexWasCreated;
+            Mutex newMutex = new Mutex(true, "SyncFileMutex", out mutexWasCreated);
+            if (!Directory.Exists(@"c:\temp"))
+            {
+                Directory.CreateDirectory(@"c:\temp");
+            }
+            Console.WriteLine("Starting writing");
+            for (int i = 0; i < 10000; i++)
+            {
+                if (!mutexWasCreated)
+                {
+                    newMutex.WaitOne();
+                }
+                using (StreamWriter streamWriter = new StreamWriter(@"c:\temp\data.txt", true))
+                {
+                    Console.WriteLine($"Current process is {Process.GetCurrentProcess().Id},Line Number {i}");
+                    streamWriter.WriteLine($"Current process is {Process.GetCurrentProcess().Id},Line Number {i}");
+                }
+                newMutex.ReleaseMutex();
+            }
+            Console.WriteLine("Finished writing");
+        }
+    }
+}
