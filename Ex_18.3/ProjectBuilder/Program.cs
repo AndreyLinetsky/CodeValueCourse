86562f5fc15ab5c8ecd382513aea8d57febfd74e
Andrey <Andrey@DESKTOP-9US8I6T>
2016-08-09 16:45:41 +0300

    projects

---
+++ Ex_18.3/ProjectBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Build projects sequentially
+            for (int i = 1; i <= 8; i++)
+            {
+                Project currProject = new Project(i);
+            }
+            // Build projects using tasks
+            Task firstTask = Task.Run(() => { Project currProject = new Project(1); });
+            Task secondTask = Task.Run(() => { Project currProject = new Project(2); });
+            Task thirdTask = Task.Run(() => { Project currProject = new Project(3); });
+            Task fourthTask = firstTask.ContinueWith(t => { Project currProject = new Project(4); });
+            Task fifthTask = Task.Factory.ContinueWhenAll(new Task[] { firstTask, secondTask, thirdTask }, t => { Project currProject = new Project(5); });
+            Task sixthTask = Task.Factory.ContinueWhenAll(new Task[] { thirdTask, fourthTask }, t => { Project currProject = new Project(6); });
+            Task seventhTask = Task.Factory.ContinueWhenAll(new Task[] { firstTask, sixthTask }, t => { Project currProject = new Project(7); });
+            Task eightTask = fifthTask.ContinueWith(t => { Project currProject = new Project(8); });
+            Task.WaitAll(seventhTask, eightTask);
+        }
+    }
+}
---
+++ Ex_18.3/ProjectBuilder/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace ProjectBuilder
+{
+    public class Project
+    {
+        public Project(int id)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine($"Finished bulding project {id}");
+        }
+    }
+}
