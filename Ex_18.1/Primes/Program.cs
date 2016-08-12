50264f45086c869eb029e1b0916196631dd41d49
Andrey <Andrey@DESKTOP-9US8I6T>
2016-08-12 19:35:27 +0300

    final 18.1

--- Ex_18.1/Primes/Program.cs
+++ Ex_18.1/Primes/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Primes
 {
@@ -22,19 +23,16 @@ namespace Primes
                     int.TryParse(secondInput, out secondNum) &&
                     int.TryParse(thirdInput, out thirdNum))
                 {
-                    List<int> primeList = CalcPrimes(firstNum, secondNum, thirdNum);
-                    if (primeList.Count > 0)
-                    {
-                        Console.WriteLine("Prime numbers in the given range are:");
-                        foreach (int number in primeList)
-                        {
-                            Console.WriteLine(number);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("No prime numbers were found in the given range");
-                    }
+                    // Check seq performance
+                    Stopwatch seqWatch = Stopwatch.StartNew();
+                    CalcPrimes(firstNum, secondNum, 1);
+                    seqWatch.Stop();
+                    Console.WriteLine($"Seq worked for {seqWatch.ElapsedMilliseconds} ms");
+                    // Check par performance
+                    Stopwatch parWatch = Stopwatch.StartNew();
+                    CalcPrimes(firstNum, secondNum, thirdNum);
+                    parWatch.Stop();
+                    Console.WriteLine($"Par worked for {parWatch.ElapsedMilliseconds} ms");
                 }
                 else
                 {
@@ -60,29 +58,29 @@ namespace Primes
                 firstNum = secondNum;
                 secondNum = tempNumber;
             }
-            Parallel.For(firstNum, secondNum + 1, new ParallelOptions { MaxDegreeOfParallelism = maxDegree }, (i) =>
-                {
-                    isPrime = true;
+            Parallel.For(firstNum, secondNum + 1, new ParallelOptions { MaxDegreeOfParallelism = maxDegree }, i =>
+            {
+                isPrime = true;
                 // Filter only possible prime values
                 if (i > 1)
+                {
+                    for (int j = 2; j <= i / 2; j++)
                     {
-                        for (int j = 2; j <= i / 2; j++)
+                        if (i % j == 0)
                         {
-                            if (i % j == 0)
-                            {
-                                isPrime = false;
-                                break;
-                            }
-                        }
-                        if (isPrime)
-                        {
-                            lock (sync)
-                            {
-                                primeList.Add(i);
-                            }
+                            isPrime = false;
+                            break;
                         }
                     }
-                });
+                    if (isPrime)
+                    {
+                        lock (sync)
+                        {
+                            primeList.Add(i);
+                        }
+                    }
+                }
+            });
             return primeList;
         }
     }
