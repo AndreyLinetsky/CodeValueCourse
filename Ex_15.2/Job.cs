4ef30ae66c8c7ae79b31865661c36fe2f7acd73e
Andrey <Andrey@DESKTOP-9US8I6T>
2016-08-12 21:52:55 +0300

    15.2 final

---
+++ Ex_15.2/Job.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Jobs
+{
+    static class NativeJob
+    {
+        [DllImport("kernel32")]
+        public static extern IntPtr CreateJobObject(IntPtr sa, string name);
+
+        [DllImport("kernel32", SetLastError = true)]
+        public static extern bool AssignProcessToJobObject(IntPtr hjob, IntPtr hprocess);
+
+        [DllImport("kernel32")]
+        public static extern bool CloseHandle(IntPtr h);
+
+        [DllImport("kernel32")]
+        public static extern bool TerminateJobObject(IntPtr hjob, uint code);
+    }
+
+    public class Job
+    {
+        private long _size;
+        private string _name;
+        public Job(string name, long sizeInByte)
+        {
+            if (sizeInByte < 1)
+            {
+                throw new ArgumentOutOfRangeException("Size", "Size must be greater than 0");
+            }
+            _size = sizeInByte;
+            _name = name;
+            GC.AddMemoryPressure(_size);
+            Console.WriteLine($"{_name} was created", _name);
+        }
+
+        public Job()
+            : this(null, 1)
+        {
+        }
+
+        ~Job()
+        {
+            if (_size > 0)
+            {
+                GC.RemoveMemoryPressure(_size);
+                Console.WriteLine($"{_name} was released", _name);
+            }
+        }
+    }
+}
---
+++ Ex_15.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Jobs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                try
+                {
+                    var job = new Job("Job number " + i, i * 10000000);
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
